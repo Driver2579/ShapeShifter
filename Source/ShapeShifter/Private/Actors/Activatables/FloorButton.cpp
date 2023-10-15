981fdcec4c3b8ef1c12d161a454a5be421e35cb3
Driver2579 <driver2579@gmail.com>
2023-10-15 14:47:24 +0300

    Moved Actors with Activatable interface to a separate folder

--- Source/ShapeShifter/Private/Actors/Activatables/FloorButton.cpp
+++ Source/ShapeShifter/Private/Actors/Activatables/FloorButton.cpp
@@ -1,6 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Actors/FloorButton.h"
+#include "Actors/Activatables/FloorButton.h"
 
 AFloorButton::AFloorButton()
 {
--- Source/ShapeShifter/Private/Actors/Activatables/Laser.cpp
+++ Source/ShapeShifter/Private/Actors/Activatables/Laser.cpp
@@ -1,6 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Actors/Laser.h"
+#include "Actors/Activatables/Laser.h"
 
 #include "NiagaraComponent.h"
 #include "NiagaraFunctionLibrary.h"

7aacb28e364057db0b2fc7c22ab7d47b470a992b
TheSheleng <128699288+TheSheleng@users.noreply.github.com>
2023-10-29 11:22:52 +0200

    Refactoring moving platforms

---
+++ Source/ShapeShifter/Private/Actors/Activatables/MovingPlatform.cpp
@@ -0,0 +1,140 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/Activatables/MovingPlatform.h"
+#include "Components/SplineComponent.h"
+
+DEFINE_LOG_CATEGORY_STATIC(MovingPlatform, All, All);
+
+AMovingPlatform::AMovingPlatform()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	
+	SplineComponent = CreateDefaultSubobject<USplineComponent>(TEXT("Spline Component"));
+	RootComponent = SplineComponent;
+	
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh Component"));
+	MeshComponent->SetupAttachment(RootComponent);
+}
+
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (!MovementCurve)
+	{
+		UE_LOG(MovingPlatform, Error, TEXT("%s: Movement Curve not found"), *GetName());
+
+		return;
+	}
+
+	// Duplicate curve for editing and using
+	MovementCurve = DuplicateObject(MovementCurve, nullptr);
+
+	// Changing Animation Time
+	for (FRichCurveKey& Key : MovementCurve->FloatCurve.Keys)
+	{
+		Key.Time *= MoveTime;
+	}
+
+	// Calibrate the spline position of mesh
+	ProcessMovementTimeline(MovementCurve->FloatCurve.Keys[0].Value);
+
+	// Calibrate the world position of mesh 
+	const FVector SplineLocation = SplineComponent->GetLocationAtSplinePoint(0, ESplineCoordinateSpace::World);
+	MeshComponent->SetWorldLocation(SplineLocation);
+
+	FOnTimelineFloat ProgressFunction;
+	ProgressFunction.BindUFunction(this, TEXT("ProcessMovementTimeline"));
+	MovementTimeline.AddInterpFloat(MovementCurve, ProgressFunction);
+
+	MovementTimeline.SetTimelineLengthMode(TL_LastKeyFrame);
+	MovementTimeline.SetLooping(bLoop);
+}
+
+void AMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if(MovementTimeline.IsPlaying())
+	{
+		MovementTimeline.TickTimeline(DeltaTime);	
+	}
+}
+
+void AMovingPlatform::ProcessMovementTimeline(float Value)
+{
+	const float Distance = Value * SplineComponent->GetSplineLength();
+
+	const FVector CurrentSplineLocation = SplineComponent->GetLocationAtDistanceAlongSpline(
+		Distance, ESplineCoordinateSpace::World);
+
+	if (bRotate)
+	{
+		FRotator CurrentSplineRotation = SplineComponent->GetRotationAtDistanceAlongSpline(
+			Distance, ESplineCoordinateSpace::World);
+		CurrentSplineRotation.Pitch = 0.f;
+
+		MeshComponent->SetWorldLocationAndRotation(CurrentSplineLocation, CurrentSplineRotation);
+	}
+	else
+	{
+		MeshComponent->SetWorldLocation(CurrentSplineLocation);
+	}
+}
+
+
+void AMovingPlatform::Activate()
+{
+	bIsActive = true;
+
+	if (MoveTime == 0)
+	{
+		UE_LOG(MovingPlatform, Warning, TEXT("%s: Move Time is 0"), *GetName());
+
+		return;
+	}
+
+	GetWorldTimerManager().ClearTimer(MoveTimerHandle);
+
+	// Start with or without delay
+	if (StartDelay == 0)
+	{
+		MovementTimeline.Play();
+	}
+	else
+	{
+		GetWorldTimerManager().SetTimer(MoveTimerHandle, [this]() {
+			MovementTimeline.Play();
+		}, StartDelay, false);
+	}
+}
+
+void AMovingPlatform::Deactivate()
+{
+	bIsActive = false;
+	MovementTimeline.Stop();
+
+	GetWorldTimerManager().ClearTimer(MoveTimerHandle);
+
+	if (!bLoop)
+	{
+		// Reverse with or without delay
+		if (EndDelay == 0)
+		{
+			MovementTimeline.Reverse();
+		}
+		else
+		{
+			GetWorldTimerManager().SetTimer(MoveTimerHandle, [this]
+			{
+				MovementTimeline.Reverse();
+			}, EndDelay, false);
+		}
+	}
+}
+
+bool AMovingPlatform::IsActive() const
+{
+	return bIsActive;
+}
---
+++ Source/ShapeShifter/Public/Actors/Activatables/MovingPlatform.h
@@ -0,0 +1,72 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Interfaces/Activatable.h"
+#include "Components/TimelineComponent.h"
+#include "MovingPlatform.generated.h"
+
+class USplineComponent;
+class UBoxComponent;
+
+UCLASS()
+class SHAPESHIFTER_API AMovingPlatform : public AActor, public IActivatable
+{
+	GENERATED_BODY()
+	
+public:
+	AMovingPlatform();
+
+	virtual void Tick(float DeltaTime) override;
+
+	// Implementing the IActivatable
+	virtual void Activate() override;
+	virtual void Deactivate() override;
+	virtual bool IsActive() const override;
+	
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "MovingPlatform")
+	USplineComponent* SplineComponent;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "MovingPlatform")
+	UStaticMeshComponent* MeshComponent;
+	
+	/**
+	 * Should display the dependence of position on the route on time
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "MovingPlatform")
+	UCurveFloat* MovementCurve;
+
+	// Time it takes the platform to complete the route
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "MovingPlatform", meta = (ClampMin = "0"))
+	float MoveTime = 1;
+
+	// Time it takes the platform to complete the route
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "MovingPlatform", meta = (ClampMin = "0"))
+	float StartDelay = 0;
+
+	// Delay before starting to move back
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "MovingPlatform", meta = (ClampMin = "0"))
+	float EndDelay = 0;
+
+	// Should the platform be turned towards the route
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "MovingPlatform")
+	bool bRotate = false;
+
+	// Infinite movement when activated and stop in place when deactivated
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "MovingPlatform")
+	bool bLoop = false;
+
+	// 
+	UFUNCTION()
+	void ProcessMovementTimeline(float Value);
+
+private:
+	bool bIsActive = false;
+	FTimeline MovementTimeline;
+	FTimerHandle MoveTimerHandle;
+};

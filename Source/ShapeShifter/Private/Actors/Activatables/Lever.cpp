65ae0f221b7e74aeef2ab911b5334340d35e4a98
Driver2579 <driver2579@gmail.com>
2023-11-06 01:30:38 +0200

    Implemented Lever rotation on activation/deactivation. Increased BP_BallPawn JumpImpulse.

---
+++ Source/ShapeShifter/Private/Actors/Activatables/Lever.cpp
@@ -0,0 +1,140 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Actors/Activatables/Lever.h"
+
+#include "Components/BoxComponent.h"
+
+ALever::ALever()
+{
+ 	// Set this actor to call Tick() every frame
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+
+	BaseMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Base Mesh"));
+	BaseMeshComponent->SetupAttachment(RootComponent);
+
+	LeverMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Lever Mesh"));
+	LeverMeshComponent->SetupAttachment(RootComponent);
+
+	// Enable Hit Events
+	LeverMeshComponent->SetNotifyRigidBodyCollision(true);
+
+	ActivateZoneComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("Activate Zone"));
+	ActivateZoneComponent->SetupAttachment(LeverMeshComponent);
+
+	DeactivateZoneComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("Deactivate Zone"));
+	DeactivateZoneComponent->SetupAttachment(LeverMeshComponent);
+
+	ActivateZoneComponent->SetCollisionProfileName(TEXT("OverlapOnlyPawn"));
+	DeactivateZoneComponent->SetCollisionProfileName(TEXT("OverlapOnlyPawn"));
+}
+
+void ALever::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+	if (bActive)
+	{
+		LeverMeshComponent->SetRelativeRotation(LeverMeshActiveRotation);
+	}
+	else
+	{
+		LeverMeshComponent->SetRelativeRotation(LeverMeshInactiveRotation);
+	}
+}
+
+void ALever::BeginPlay()
+{
+	Super::BeginPlay();
+
+	LeverMeshComponent->OnComponentHit.AddDynamic(this, &ALever::OnLeverMeshComponentHit);
+}
+
+void ALever::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	// Return to safe performance if we don't need to rotate
+	if (!bRotate)
+	{
+		return;
+	}
+
+	// Get LeverMeshTargetRotation by bActive state
+	const FRotator& LeverMeshTargetRotation = bActive ? LeverMeshActiveRotation : LeverMeshInactiveRotation;
+
+	const FRotator& NewRotation = FMath::RInterpTo(LeverMeshComponent->GetRelativeRotation(),
+		LeverMeshTargetRotation, DeltaTime, RotationSpeed);
+
+	LeverMeshComponent->SetRelativeRotation(NewRotation);
+
+	// We need this because the default tolerance in Equals function is too small and it could fail sometimes
+	const float Tolerance = 0.001;
+
+	if (LeverMeshComponent->GetRelativeRotation().Equals(LeverMeshTargetRotation, Tolerance))
+	{
+		bRotate = false;
+	}
+}
+
+void ALever::OnLeverMeshComponentHit(UPrimitiveComponent* HitComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	// We don't need to handle OtherComps without physics
+	if (!IsValid(OtherComp) || !OtherComp->IsSimulatingPhysics())
+	{
+		return;
+	}
+
+	// Get ZVelocity of OtherComp
+	const float ZVelocity = OtherComp->GetComponentVelocity().Z;
+
+	// Activate the Lever if OtherComp hit the top of it and clear OtherComp velocity
+	if (ZVelocity > VelocityToSwitchActivation && OtherComp->IsOverlappingComponent(ActivateZoneComponent))
+	{
+		Activate();
+
+		OtherComp->SetAllPhysicsLinearVelocity(FVector::ZeroVector);
+	}
+	// Deactivate the Lever if OtherComp hit the bottom of it and clear OtherComp velocity
+	else if (ZVelocity < -VelocityToSwitchActivation && OtherComp->IsOverlappingComponent(DeactivateZoneComponent))
+	{
+		Deactivate();
+
+		OtherComp->SetAllPhysicsLinearVelocity(FVector::ZeroVector);
+	}
+}
+
+bool ALever::IsActive() const
+{
+	return bActive;
+}
+
+void ALever::Activate()
+{
+	// We don't need to do anything if Lever is already inactive
+	if (bActive)
+	{
+		return;
+	}
+
+	bActive = true;
+
+	// Turn on LeverMeshComponent rotation because activation was switched
+	bRotate = true;
+}
+
+void ALever::Deactivate()
+{
+	// We don't need to do anything if Lever is already inactive
+	if (!bActive)
+	{
+		return;
+	}
+
+	bActive = false;
+
+	// Turn on LeverMeshComponent rotation because activation was switched
+	bRotate = true;
+}
---
+++ Source/ShapeShifter/Public/Actors/Activatables/Lever.h
@@ -0,0 +1,69 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Interfaces/Activatable.h"
+#include "Lever.generated.h"
+
+class UBoxComponent;
+
+UCLASS()
+class SHAPESHIFTER_API ALever : public AActor, public IActivatable
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this actor's properties
+	ALever();
+
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	virtual bool IsActive() const override final;
+
+	virtual void Activate() override;
+	virtual void Deactivate() override;
+
+protected:
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UStaticMeshComponent* BaseMeshComponent;
+	
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UStaticMeshComponent* LeverMeshComponent;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UBoxComponent* ActivateZoneComponent;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UBoxComponent* DeactivateZoneComponent;
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	virtual void OnLeverMeshComponentHit(UPrimitiveComponent* HitComponent, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+private:
+	UPROPERTY(EditAnywhere, Category = "Activation")
+	bool bActive = false;
+
+	// Velocity needed for physics component to activate or deactivate the lever
+	UPROPERTY(EditDefaultsOnly, Category = "Activation")
+	float VelocityToSwitchActivation = 10;
+
+	bool bRotate = false;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Lever Rotation")
+	FRotator LeverMeshActiveRotation = FRotator(0, 0, 110);
+
+	UPROPERTY(EditDefaultsOnly, Category = "Lever Rotation")
+	FRotator LeverMeshInactiveRotation = FRotator(0, 0, 70);
+
+	UPROPERTY(EditDefaultsOnly, Category = "Lever Rotation")
+	float RotationSpeed = 20;
+};

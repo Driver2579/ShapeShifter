1d4c4e0bc10a76371b01aaf8dd67eb4820463091
Driver2579 <driver2579@gmail.com>
2023-11-10 23:15:43 +0200

    Added background images and description to loading screen. Updated Target.cs files.

--- Source/ShapeShifterEditor.Target.cs
+++ Source/ShapeShifterEditor.Target.cs
@@ -1,15 +1,14 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
-using System.Collections.Generic;
 
 public class ShapeShifterEditorTarget : TargetRules
 {
 	public ShapeShifterEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("ShapeShifter");
 	}
 }

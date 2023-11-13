eed0b35af53a34ce0ec0e30c4532df5d1910075d
TheSheleng <128699288+TheSheleng@users.noreply.github.com>
2023-11-13 23:38:41 +0200

    Fix by review #3

--- Source/ShapeShifter/Private/Actors/Activatables/Jumppad.cpp
+++ Source/ShapeShifter/Private/Actors/Activatables/Jumppad.cpp
@@ -1,6 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Actors/Activatables/Jumppad.h"
+#include "Actors/Activatables/JumpPad.h"
 
 #include "Components/BoxComponent.h"
 
